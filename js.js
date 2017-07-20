0344ff10c260ce96e04f6854bb2043a822873775
Josselin <josselin.td@gmail.com>
2017-07-20 23:06:04 +0200

    Style status

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 var navPath = 'nav .main span';
 
+var model = JSON.parse(localStorage.getItem('model')) || {};
+
 $(function() {
 	$('body').on('click', navPath, function() {
 		$(navPath).removeClass('active');
@@ -7,4 +9,23 @@ $(function() {
 
 		$('body').attr('class', $(this).attr('class'));
 	});
+
+	$('body').on('click', '[data-prompt]', function() {
+		var attrName = $(this).data('prompt');
+		var newValue = prompt('Quelle est la valeur de ' + attrName + ' ?', model[attrName] || '');
+
+		if (newValue !== null) {
+			model[attrName] = newValue;
+			updateModel();
+		}
+	});
+
+	updateModel();
 });
+
+function updateModel() {
+	localStorage.setItem('model', JSON.stringify(model));
+	Object.keys(model).forEach(function(attrName) {
+		$('[data-html=' + attrName + ']').html(model[attrName]);
+	});
+}
