8d22d3f945f45b7b1622ee0b9a68d33cd63c0913
Josselin <josselin.td@gmail.com>
2017-08-10 16:34:58 +0200

    Add Chrono

--- app/routing.js
+++ app/routing.js
@@ -8,6 +8,9 @@ $(function() {
 
 	$('body').attr('class', 'infos');
 
+	$('body').on('click', '[data-chrono-plus]', chronoPlus);
+	$('body').on('click', '[data-chrono-moins]', chronoMoins);
+
 	startTime();
 });
 
@@ -29,4 +32,47 @@ function startTime() {
   t = setTimeout(function() {
     startTime()
   }, 500);
+}
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+
+	if (m<0) {
+		document.getElementById('timer').innerHTML = "00:00";
+	} else {
+	  document.getElementById('timer').innerHTML = m + ":" + s;
+	  setTimeout(startTimer, 1000);
+	}
+}
+
+function chronoPlus() {
+	var presentTime = document.getElementById('timer').innerHTML;
+	var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond(+timeArray[1]);
+
+	m++;
+
+	document.getElementById('timer').innerHTML = m + ":" + s;
+}
+
+function chronoMoins() {
+	var presentTime = document.getElementById('timer').innerHTML;
+	var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond(+timeArray[1]);
+
+	m--;
+
+	document.getElementById('timer').innerHTML = m + ":" + s;
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
 }
