1c44feca58262a797da7ad7f1fe9b1b5b78db86d
Josselin <josselin.td@gmail.com>
2017-08-10 15:56:40 +0200

    Add hour

--- app/routing.js
+++ app/routing.js
@@ -7,4 +7,26 @@ $(function() {
 	});
 
 	$('body').attr('class', 'infos');
+
+	startTime();
 });
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  document.getElementById('time').innerHTML = h + ":" + m;
+  t = setTimeout(function() {
+    startTime()
+  }, 500);
+}
